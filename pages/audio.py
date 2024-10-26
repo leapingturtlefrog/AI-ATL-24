a0d5e00b16cafe92d96ea07f108235e253cd4c31
CalebBunch <rcbcg123@gmail.com>
2024-10-25 21:15:01 -0400

    cleanups

--- pages/audio.py
+++ pages/audio.py
@@ -9,27 +9,23 @@ st.markdown('''<style>.stAudio {height: 45px;}</style>''',
 st.markdown('''<style>.css-v37k9u a {color: #0000ff;}</style>''',
             unsafe_allow_html=True) 
 
-
 def audiorec_demo_app():
 
-    st.title('streamlit audio recorder')
+    st.title("streamlit audio recorder")
 
     wav_audio_data = st_audiorec()
 
     col_info, col_space = st.columns([0.57, 0.43])
     with col_info:
-        st.write('\n') 
-        st.write('\n')
-        st.write('The .wav audio data, as received in the backend Python code,'
-                 ' will be displayed below this message as soon as it has'
-                 ' been processed. [This informative message is not part of'
-                 ' the audio recorder and can be removed easily] 🎈')
+        st.write("\n") 
+        st.write("\n")
+        st.write("The .wav audio data is below")
 
     if wav_audio_data is not None:
-        col_playback, col_space = st.columns([0.58,0.42])
+        col_playback, col_space = st.columns([0.58, 0.42])
         with col_playback:
-            st.audio(wav_audio_data, format='audio/wav')
+            st.audio(wav_audio_data, format="audio/wav")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     audiorec_demo_app()
