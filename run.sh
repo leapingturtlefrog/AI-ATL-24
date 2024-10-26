e45362756d433be8c1c27d341a4be706a03aafe1
CalebBunch <rcbcg123@gmail.com>
2024-10-25 21:59:38 -0400

    changes to run.sh

--- run.sh
+++ run.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
-# Run with the command './run.sh'
-# Builds the Docker image and runs a new container.
-
+OS_NAME=$(uname -s)
 IMAGE_NAME=image_a
 CONTAINER_NAME=container_a
-if [ "$(docker ps -aq -f name=$CONTAINER_NAME)" ]; then
-        echo "Starting existing container $CONTAINER_NAME..."
-        docker start $CONTAINER_NAME
+
+if [ "$OS_NAME" == "Linux" ]; then
+    DOCKER_COMMAND="docker run -t -i -e "PULSE_SERVER=${PULSE_SERVER}" -v /mnt/wslg/:/mnt/wslg/ --name $CONTAINER_NAME -p 8501:8501 --rm -it --privileged $IMAGE_NAME"
+elif [ "$OS_NAME" == "Darwin" ]; then
+    DOCKER_COMMAND="docker run --name $CONTAINER_NAME -p 8501:8501 --rm -it $IMAGE_NAME"
 else
-        echo "Building the Docker image"
-        docker build -t $IMAGE_NAME .
-        echo "Running a new container"
-        docker run --name $CONTAINER_NAME -p 8501:8501 --rm -it --privileged $IMAGE_NAME
+    echo "Unsupported operating system: $OS_NAME"
+    exit 1
 fi
+
+if [ "$(docker ps -aq -f name=$CONTAINER_NAME)" ]; then    echo "Starting existing container $CONTAINER_NAME..."
+    docker start $CONTAINER_NAME
+else
+    echo "Building the Docker image"
+    docker build -t $IMAGE_NAME .
+    echo "Running a new container"
+    eval $DOCKER_COMMAND
+fi
+
