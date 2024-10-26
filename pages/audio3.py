60e33499c23bec8c6cf45fb0af7ac486f96c8f8d
vingupta22 <vguptax1@gmail.com>
2024-10-26 12:30:11 -0400

    clean up files

--- pages/audio3.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-audio_value = st.experimental_audio_input("Record a voice message")
-
-if audio_value:
-    st.audio(audio_value)
--- pages/database.py
+++
@@ -1,6 +0,0 @@
-import firebase_admin
-from firebase_admin import db, credentials
-cred = credentials.Certificate("credentials.json")
-firebase_admin.initialize_app(cred,{"databaseURL": "https://ai-atl-f34bf-default-rtdb.firebaseio.com/"})
-ref = db.reference("/")
-ref.get()
