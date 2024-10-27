21274934f742b77106741dad04c53f1df8fc0d36
CalebBunch <rcbcg123@gmail.com>
2024-10-27 00:29:14 -0400

    home updates

--- components/home.py
+++ components/home.py
@@ -1,23 +1,25 @@
+import random
 import pandas as pd
 from components.start_session import start_session_page
 
 def home_page(st, metrics, name):
     st.title(f"Welcome Back, {name}!")
 
-    # TODO: fix session logs
-    # session_logs = ""
 
-    # TODO: this call is working but the old page is still visible
-    # st.button("Start New Session", on_click=lambda: start_session_page(st, session_logs))
-
-    st.subheader("Recent Sessions")
 
     metrics_df = pd.DataFrame.from_dict(metrics, orient="index", columns=["Time Spent", "Pictures Seen", "Responses"])
 
     metrics_df.reset_index(inplace=True)
     metrics_df.rename(columns={"index": "Session"}, inplace=True)
 
+    metrics_df['Session'] = metrics_df['Session'].astype(int)
+    metrics_df['Session'] = "Session " + metrics_df['Session'].astype(str)
+
+    metrics_df['Session Number'] = metrics_df['Session'].str.extract('(\d+)').astype(int)
+    metrics_df.sort_values('Session Number', inplace=True)
+
     metrics_df.set_index("Session", inplace=True)
+    metrics_df.drop(columns='Session Number', inplace=True)
 
     col1, col2 = st.columns(2)
 
@@ -29,3 +31,4 @@ def home_page(st, metrics, name):
         st.write("")
         st.subheader("Session Trends")
         st.line_chart(metrics_df)
+
