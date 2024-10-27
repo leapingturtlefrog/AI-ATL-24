97daa7f3bb33ab9ab9914d7066d202f5d3922279
Caleb Bunch <100161536+CalebBunch@users.noreply.github.com>
2024-10-26 22:57:41 -0400

    Update home.py

--- components/home.py
+++ components/home.py
@@ -1,11 +1,31 @@
-import streamlit as st
+import pandas as pd
+from components.start_session import start_session_page
 
 def home_page(st, metrics, name):
     st.title(f"Welcome Back, {name}!")
+
+    # TODO: fix session logs
+    # session_logs = ""
+
+    # TODO: this call is working but the old page is still visible
+    # st.button("Start New Session", on_click=lambda: start_session_page(st, session_logs))
+
     st.subheader("Recent Sessions")
 
-    # key is date, valiue is metrics (list[time spent, pictures seen, responses, etc.])
-    for key, value in metrics.items():
-        st.write(f"{key}: {value}")
+    metrics_df = pd.DataFrame.from_dict(metrics, orient="index", columns=["Time Spent", "Pictures Seen", "Responses"])
 
+    metrics_df.reset_index(inplace=True)
+    metrics_df.rename(columns={"index": "Session"}, inplace=True)
 
+    metrics_df.set_index("Session", inplace=True)
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+        st.subheader("Metrics Overview")
+        st.dataframe(metrics_df)
+
+    with col2:
+        st.write("")
+        st.subheader("Session Trends")
+        st.line_chart(metrics_df)
